6fdcfa7c944c25f28e5c1e09e1c59cf83ec63a27
Vektor Dewanto <vektor.dewanto@gmail.com>
2019-03-03 14:48:10 +1000

    fix

--- main.py
+++ main.py
@@ -4,60 +4,19 @@ import torch
 import gym
 import argparse
 import os
-
 import utils
-import TD3
 import OurDDPG
-import DDPG
-
-
-# # Runs policy for X episodes and returns average reward
-# def evaluate_policy(policy, eval_episodes=10):
-# 	avg_reward = 0.
-# 	for _ in range(eval_episodes):
-# 		obs = env.reset()
-# 		done = False
-# 		while not done:
-# 			action = policy.select_action(np.array(obs))
-# 			obs, reward, done, _ = env.step(action)
-# 			avg_reward += reward
-
-# 	avg_reward /= eval_episodes
-
-# 	print("---------------------------------------")
-# 	print("Evaluation over {} episodes: {}".format(eval_episodes, avg_reward))
-# 	print("---------------------------------------")
-# 	return avg_reward
-
 
 if __name__ == "__main__":
-
 	parser = argparse.ArgumentParser()
-	parser.add_argument("--policy_name", default="TD3")					# Policy name
 	parser.add_argument("--env_name", default="Reacher-v2")			# OpenAI gym environment name
 	parser.add_argument("--seed", default=0, type=int)					# Sets Gym, PyTorch and Numpy seeds
-	parser.add_argument("--start_timesteps", default=1e4, type=int)		# How many time steps purely random policy is run for
-	parser.add_argument("--eval_freq", default=5e3, type=float)			# How often (time steps) we evaluate
 	parser.add_argument("--max_timesteps", default=1e6, type=float)		# Max time steps to run environment for
-	parser.add_argument("--save_models", action="store_true")			# Whether or not models are saved
 	parser.add_argument("--expl_noise", default=0.1, type=float)		# Std of Gaussian exploration noise
 	parser.add_argument("--batch_size", default=100, type=int)			# Batch size for both actor and critic
 	parser.add_argument("--discount", default=0.99, type=float)			# Discount factor
 	parser.add_argument("--tau", default=0.005, type=float)				# Target network update rate
-	# parser.add_argument("--policy_noise", default=0.2, type=float)		# Noise added to target policy during critic update
-	# parser.add_argument("--noise_clip", default=0.5, type=float)		# Range to clip target policy noise
-	# parser.add_argument("--policy_freq", default=2, type=int)			# Frequency of delayed policy updates
 	args = parser.parse_args()
-
-	file_name = "%s_%s_%s" % (args.policy_name, args.env_name, str(args.seed))
-	print("---------------------------------------")
-	print("Settings: %s" % (file_name))
-	print("---------------------------------------")
-
-	if not os.path.exists("./results"):
-		os.makedirs("./results")
-	if args.save_models and not os.path.exists("./pytorch_models"):
-		os.makedirs("./pytorch_models")
 
 	env = gym.make(args.env_name)
 
@@ -71,15 +30,8 @@ if __name__ == "__main__":
 	max_action = float(env.action_space.high[0])
 
 	# Initialize policy
-	if args.policy_name == "TD3": policy = TD3.TD3(state_dim, action_dim, max_action)
-	elif args.policy_name == "OurDDPG": policy = OurDDPG.DDPG(state_dim, action_dim, max_action)
-	else: raise NotImplementedError
-	# elif args.policy_name == "DDPG": policy = DDPG.DDPG(state_dim, action_dim, max_action)
-
+	policy = OurDDPG.DDPG(state_dim, action_dim, max_action)
 	replay_buffer = utils.ReplayBuffer()
-
-	# Evaluate untrained policy
-	# evaluations = [evaluate_policy(policy)]
 
 	total_timesteps = 0
 	timesteps_since_eval = 0
@@ -87,24 +39,10 @@ if __name__ == "__main__":
 	done = True
 
 	while total_timesteps < args.max_timesteps:
-
 		if done:
-
 			if total_timesteps != 0:
-				print("Total T: {} Episode Num: {} Episode T: {} Reward: {}".format(total_timesteps, episode_num, episode_timesteps, episode_reward))
-				if args.policy_name == "TD3":
-					raise NotImplementedError
-					# policy.train(replay_buffer, episode_timesteps, args.batch_size, args.discount, args.tau, args.policy_noise, args.noise_clip, args.policy_freq)
-				else:
-					policy.train(replay_buffer, episode_timesteps, args.batch_size, args.discount, args.tau)
-
-			# # Evaluate episode
-			# if timesteps_since_eval >= args.eval_freq:
-			# 	timesteps_since_eval %= args.eval_freq
-			# 	evaluations.append(evaluate_policy(policy))
-
-			# 	if args.save_models: policy.save(file_name, directory="./pytorch_models")
-			# 	np.save("./results/%s" % (file_name), evaluations)
+				print("Total T: {} Episode Num: {} Episode T: {} Return: {}".format(total_timesteps, episode_num, episode_timesteps, episode_reward))
+				policy.train(replay_buffer, episode_timesteps, args.batch_size, args.discount, args.tau)
 
 			# Reset environment
 			obs = env.reset()
@@ -114,13 +52,9 @@ if __name__ == "__main__":
 			episode_num += 1
 
 		# Select action randomly or according to policy
-		if total_timesteps < args.start_timesteps:
-			raise NotImplementedError
-			# action = env.action_space.sample()
-		else:
-			action = policy.select_action(np.array(obs))
-			if args.expl_noise != 0:
-				action = (action + np.random.normal(0, args.expl_noise, size=env.action_space.shape[0])).clip(env.action_space.low, env.action_space.high)
+		action = policy.select_action(np.array(obs))
+		if args.expl_noise != 0:
+			action = (action + np.random.normal(0, args.expl_noise, size=env.action_space.shape[0])).clip(env.action_space.low, env.action_space.high)
 
 		# Perform action
 		new_obs, reward, done, _ = env.step(action)
@@ -135,8 +69,3 @@ if __name__ == "__main__":
 		episode_timesteps += 1
 		total_timesteps += 1
 		timesteps_since_eval += 1
-
-	# # Final evaluation
-	# evaluations.append(evaluate_policy(policy))
-	# if args.save_models: policy.save("%s" % (file_name), directory="./pytorch_models")
-	# np.save("./results/%s" % (file_name), evaluations)
--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 ./main.py \
---policy_name OurDDPG \
---start_timesteps 0 \
 --max_timesteps 10000
