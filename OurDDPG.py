3668d05624a4f72e4dcb70197e3b88a364e244bc
Vektor Dewanto <vektor.dewanto@gmail.com>
2019-03-04 18:18:33 +1000

    trainidx

--- OurDDPG.py
+++ OurDDPG.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
@@ -10,34 +11,34 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # Paper: https://arxiv.org/abs/1509.02971
 
 
-class Actor(nn.Module):
-	def __init__(self, state_dim, action_dim, max_action):
-		super(Actor, self).__init__()
-		self.l1 = nn.Linear(state_dim, 400)
-		self.l2 = nn.Linear(400, 300)
-		self.l3 = nn.Linear(300, action_dim)
+# class Actor(nn.Module):
+# 	def __init__(self, state_dim, action_dim, max_action):
+# 		super(Actor, self).__init__()
+# 		self.l1 = nn.Linear(state_dim, 400)
+# 		self.l2 = nn.Linear(400, 300)
+# 		self.l3 = nn.Linear(300, action_dim)
 
-	def forward(self, x):
-		# x = F.relu(self.l1(x))
-		# x = F.relu(self.l2(x))
-		x = F.tanh(self.l1(x))
-		x = F.tanh(self.l2(x))
-		x = torch.tanh(self.l3(x))
-		return x
+# 	def forward(self, x):
+# 		# x = F.relu(self.l1(x))
+# 		# x = F.relu(self.l2(x))
+# 		x = F.tanh(self.l1(x))
+# 		x = F.tanh(self.l2(x))
+# 		x = torch.tanh(self.l3(x))
+# 		return x
 
 
-class Critic(nn.Module):
-	def __init__(self, state_dim, action_dim):
-		super(Critic, self).__init__()
-		self.l1 = nn.Linear(state_dim + action_dim, 400)
-		self.l2 = nn.Linear(400, 300)
-		self.l3 = nn.Linear(300, 1)
+# class Critic(nn.Module):
+# 	def __init__(self, state_dim, action_dim):
+# 		super(Critic, self).__init__()
+# 		self.l1 = nn.Linear(state_dim + action_dim, 400)
+# 		self.l2 = nn.Linear(400, 300)
+# 		self.l3 = nn.Linear(300, 1)
 
-	def forward(self, x, u):
-		x = F.relu(self.l1(torch.cat([x, u], 1)))
-		x = F.relu(self.l2(x))
-		x = self.l3(x)
-		return x
+# 	def forward(self, x, u):
+# 		x = F.relu(self.l1(torch.cat([x, u], 1)))
+# 		x = F.relu(self.l2(x))
+# 		x = self.l3(x)
+# 		return x
 
 class DDPG(object):
 	def __init__(self, state_dim, action_dim, max_action):
@@ -56,12 +57,14 @@ class DDPG(object):
 		self.critic_target = net.ActionValueNetwork(state_dim+action_dim, [400, 300]).to(device)
 		self.critic_target.load_state_dict(self.critic.state_dict())
 
+		self.train_idx = 0
+
 	def select_action(self, state):
 		state = torch.FloatTensor(state.reshape(1, -1)).to(device)
 		return self.actor(state).cpu().data.numpy().flatten()
 
-
 	def train(self, replay_buffer, iterations, batch_size, discount, tau):
+		iterations = 50
 		for it in range(iterations):
 			# Sample replay buffer
 			x, y, u, r, d = replay_buffer.sample(batch_size)
@@ -70,22 +73,41 @@ class DDPG(object):
 			next_state = torch.FloatTensor(y).to(device)
 			done = torch.FloatTensor(d).to(device)
 			reward = torch.FloatTensor(r).to(device)
+			# print(reward)
+			# print(next_state)
 
 			# Compute the target Q value
-			target_Q = self.critic_target(next_state, self.actor_target(next_state))
-			target_Q = reward + (discount * target_Q * (1 - done)).detach()
+			# Q_next = self.critic_target(next_state, self.actor_target(next_state))
+			# target_Q = reward + (discount * Q_next * (1 - done)).detach()
+			with torch.no_grad():
+				Q_next = self.critic_target(next_state, self.actor_target(next_state))
+			target_Q = reward + (discount * Q_next * (1 - done))
+			# print(target_Q)
 
 			# Compute critic loss
 			current_Q = self.critic(state, action)
 			critic_loss = F.mse_loss(current_Q, target_Q)
+			print('critic_loss=', critic_loss)
 
 			# Optimize the critic
+			# self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), lr=3e-4)
 			self.critic_optimizer.zero_grad()
 			critic_loss.backward()
+			# for name, param in self.critic.named_parameters():
+			# 	print(param.grad)
+
 			self.critic_optimizer.step()
+			# torch.save({'critic_state_dict': self.critic.state_dict()},
+			# 	os.path.join('results', 'critic_net_'+str(it)+'.pt'))
 
 			# Compute actor loss
-			actor_loss = -self.critic(state, self.actor(state)).mean()
+			action = self.actor(state)
+			actionvalue = self.critic(state, action)
+			actor_loss = -actionvalue.mean()
+			# print(state) # same
+			# print(action) # same
+			# print(actionvalue)
+			print('actor_loss=', actor_loss)
 
 			# Optimize the actor
 			self.actor_optimizer.zero_grad()
@@ -98,3 +120,7 @@ class DDPG(object):
 
 			for param, target_param in zip(self.actor.parameters(), self.actor_target.parameters()):
 				target_param.data.copy_(tau * param.data + (1 - tau) * target_param.data)
+
+		torch.save({'actor_state_dict': self.actor.state_dict()},
+				os.path.join('results', 'actor_net_train_idx_'+str(self.train_idx)+'.pt'))
+		self.train_idx += 1
